bd8b512a6af30cdb7758e8fd5a33b48d05ac9d72
ortur-linux <addfly@yandex.ru>
2024-03-11 23:02:40 +0300

    6 homework

---
+++ PHP/new/var_and_types/homeworks/homework7.php
@@ -0,0 +1,11 @@
+<?php
+
+
+echo __LINE__ . "<br>";
+echo __FILE__ . "<br>";
+echo __FUNCTION__ . "<br>";
+echo __CLASS__ . "<br>";
+echo __METHOD__ . "<br>";
+echo __DIR__ . "<br>";
+echo PHP_VERSION . "<br>";
+echo PHP_EOL;
