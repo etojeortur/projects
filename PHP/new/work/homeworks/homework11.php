abb748794ad98beeb4a02097daf8070e9d7a468f
ortur-linux <addfly@yandex.ru>
2024-03-18 17:21:53 +0300

    10 and 11 homework

--- PHP/new/work/homeworks/homework11.php
+++ PHP/new/work/homeworks/homework11.php
@@ -1,13 +1,13 @@
 <?php
-$hour = 1;
+$hour = 13;
 $time = '';
 if ($hour > 23 | $hour < 0) {
     echo 'Ошибка: Введи время от 00 до 23 часов.';
-}elseif ($hour >= 00 & $hour <= 10 & $hour < 24 & $hour < 24 & $hour > -1) {
+}elseif ($hour >= 00 && $hour <= 10 && $hour < 24 && $hour < 24 & $hour > -1) {
     $time = 0;
-}elseif ($hour >= 11 & $hour <= 15 & $hour < 24 & $hour > -1) {
+}elseif ($hour >= 11 && $hour <= 15 && $hour < 24 && $hour > -1) {
     $time = 1;
-}elseif ($hour >= 16 & $hour <= 23 & $hour < 24 & $hour > -1) {
+}elseif ($hour >= 16 && $hour <= 23 && $hour < 24 && $hour > -1) {
     $time = 1;
 }if ($time == 1) {
     echo 'Еще рано спать';
---
+++ PHP/new/work/operators_OR_AND.php
@@ -0,0 +1,7 @@
+<?php
+$flag1 = true;
+$flag2 = true;
+
+if ($flag1 && $flag2) {
+    echo "<p>Оба флага возвращают True</p>";
+}
