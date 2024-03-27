d4fe5d13da12723479d5604a57a475048bf8bf75
ortur-linux <addfly@yandex.ru>
2024-03-27 21:13:49 +0300

    homework setters and getters

--- PHP/eshe_php/homework/homework_setters_and_getters.php
+++ PHP/eshe_php/homework/homework_setters_and_getters.php
@@ -18,3 +18,24 @@ class Animal {
         return $this->getAge();
     }
 }
+
+$pet1 = new Animal();
+echo "Мое первое домшнее животное: $pet1->type по кличке $pet1->name.";
+echo "<br>";
+
+$pet2 = new Animal();
+$pet2->setType('Dog');
+$pet2->setName('Defa');
+echo "Мое второе животное: $pet2->type по кличке $pet2->name.";
+echo "<br>";
+
+$pet3 = new Animal();
+$pet3->setType('Snake');
+$pet3->setName('Charizard');
+echo "Мое третье животное: $pet3->type по кличке $pet3->name и ей уже $pet3->age лет.";
+echo "<br>";
+
+$pet4 = new Animal;
+$pet4->setType('Fish');
+$pet4->setName('Midas');
+echo "Мое четвертое домашнее животное: $pet4->type по кличке $pet4->name.";
